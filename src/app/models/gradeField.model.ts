5ff4acafa814be08b456ca30c3e11d8d912b4675
FunTomas <krainievlc@gmail.com>
2021-05-17 17:53:46 +0200

    Created new model

---
+++ src/app/models/gradeField.model.ts
@@ -0,0 +1,11 @@
+import {GradeModel} from "./grade.model";
+
+export class GradeFieldModel {
+  constructor(
+    public grade: GradeModel,
+    public maxPercentage: string,
+    public selected: boolean
+  ) {
+  }
+
+}
