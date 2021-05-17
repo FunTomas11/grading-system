c0c05cabcfe05503e7845d9b4547e44995bcdc86
FunTomas <krainievlc@gmail.com>
2021-05-17 17:51:12 +0200

    Added default values to model

--- src/app/models/grade.model.ts
+++ src/app/models/grade.model.ts
@@ -1,9 +1,8 @@
 export class GradeModel {
   constructor(
-    public id: string,
-    public minPercentage: number,
-    public symbolicGrade: string,
+    public id: string = "",
+    public minPercentage: number = 0,
+    public symbolicGrade: string = "",
     public descriptiveGrade?: string
   ) { }
-
 }
